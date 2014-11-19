1034696026dca76a77c927610d931323be81acdf
thomashed <thomashj93@gmail.com>
2014-11-19 18:05:38 +0100

    - Got ObjectID working in test - Remeber to run 'npm install mongodb' by: Thomas

--- test/server-test/test-datalayer.js
+++ test/server-test/test-datalayer.js
@@ -1,6 +1,7 @@
 var should = require('should');
 var wikiMapper = require('../../server/source/datalayer');
 var dbConnection = require('../../server/source/db');
+var ObjectId = require('mongodb').ObjectID;
 
 describe("Testing of the DataLayer interface", function () {
 
@@ -21,7 +22,7 @@ describe("Testing of the DataLayer interface", function () {
         });
 
         var testArticle = {
-            _id: "546a5dd5e1cf19d015664e25",
+            _id: ObjectId("546a5dd5e1cf19d015664e25"),
             url: "http://en.wikipedia.org/wiki/An_American_in_Paris",
             title: "An American in Paris"
         };
@@ -30,7 +31,7 @@ describe("Testing of the DataLayer interface", function () {
             wikiMapper.getWiki(testArticle.title, function (err, article) {
 
                 // Here we check if data is equal to expected -> article
-                article.should.have.property('_id');
+                article.should.have.property('_id', testArticle._id);
                 article.should.have.property('url', testArticle.url);
                 article.should.have.property('title', testArticle.title);
                 return done();
