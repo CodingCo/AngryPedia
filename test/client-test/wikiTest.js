6a6671b7159d004fc845bdcc38fee00331698d56
robertelving <vivalaklask@gmail.com>
2014-11-20 11:11:22 +0100

    Succesful wikiTest
        Commit by: Robert

--- test/client-test/wikiTest.js
+++ test/client-test/wikiTest.js
@@ -1,25 +1,50 @@
 
 describe('WikiApp.wiki', function () {
 
+
+    var wikiMock = [
+        {
+            "title" : "TestTitle",
+            "url" : "TestURL",
+            "abstract" : "TestAbstract",
+            "categories" : [ "TestCategory" ],
+            "headings" : [
+                { "heading" : "Adjacent counties", "position" : "1" }],
+            "links" : [ "TestLink" ]
+        }
+    ];
+
+    var routeParam = "TestParam";
+
     beforeEach(function () {
         module('WikiApp.wiki')
     });
 
-    beforeEach(inject(function ($controller, $rootScope) {
-
+    beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
+        this.httpBackendMock = $httpBackend;
+        this.httpBackendMock.expectGET('getWiki/' + routeParam).
+            respond(wikiMock);
         this.scope = $rootScope.$new();
         this.ctrl = $controller('wikiCtrl', {
-            $scope: this.scope
-        });
-
+            $scope: this.scope,
+            $routeParams: {wikiTitle: routeParam}});
     }));
 
     it("should be defined", function () {
         expect(this.ctrl).toBeDefined();
     });
 
-    it("should have title on the scope eq to 'Hello'", function(){
-        expect(this.scope.title).toBe('Hello');
+    it('Should fetch 1 wiki ', function () {
+        expect(this.scope.wiki).toBeUndefined();
+        this.httpBackendMock.flush();
+        console.log(this.scope.wiki);
+        expect(this.scope.wiki.length).toEqual(1);
+    });
+
+    it('Should fetch correct wiki', function () {
+        expect(this.scope.wiki).toBeUndefined();
+        this.httpBackendMock.flush();
+        expect(JSON.stringify(this.scope.wiki)).toEqual(JSON.stringify(wikiMock));
     });
 
 });
